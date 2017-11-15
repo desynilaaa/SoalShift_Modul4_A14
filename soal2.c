16059d27622d9375e3408f97de161d0e0c03acd1
Mujahid97 <mujahidkhairuddin4@gmail.com>
2017-11-15 15:29:03 +0700

    Mencoba

--- soal2.c
+++ soal2.c
@@ -11,8 +11,59 @@
 static const char *dirpath = "/home/mujahid/Dokumen";
 
 
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+  int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		res = (filler(buf, de->d_name, &st, 0));
+			if(res!=0) break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+
 static struct fuse_operations xmp_oper = {
+	.getattr  	= xmp_getattr,
 	.readdir	= xmp_readdir,
+	.mkdir		= xmp_mkdir
 	.read		= xmp_read,
 };
 
