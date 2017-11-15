20cce778f1067171a49ad9c33af09164ef50bacc
desynilaaa <32993903+desynilaaa@users.noreply.github.com>
2017-11-15 12:59:14 +0700

    read

--- soal1.c
+++ soal1.c
@@ -75,4 +75,53 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
   return 0;
 }
 
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+ 	   struct fuse_file_info *fi)
+{
+  char fpath[1000];
+  char newFile[100];
+  if(strcmp(path,"/") == 0)
+  {
+    memcpy(newFile, path, strlen(path));
+//    path=dirpath;
+    sprintf(fpath,"%s",newFile);
+  }
+  else 
+  {
+    memcpy(newFile, path, strlen(path) - 4);
+    newFile[strlen(path) - 4] = '\0';
+
+    sprintf(fpath, "%s%s",dirpath,newFile);
+  }
+  int res = 0;
+  int fd = 0 ;
+
+  (void) fi;
+  fd = open(fpath, O_RDONLY);
+  if (fd == -1)
+    return -errno;
+
+  res = pread(fd, buf, size, offset);
+  if (res == -1)
+    res = -errno;
+
+  close(fd);
+  return res;
+}
+
+static struct fuse_operations xmp_oper = {
+  .getattr  = xmp_getattr,
+  .readdir  = xmp_readdir,
+  .read   = xmp_read,
+};
+
+int main(int argc, char *argv[])
+{
+  umask(0);
+  return fuse_main(argc, argv, &xmp_oper, NULL);
+}
+
+
+
+
 
